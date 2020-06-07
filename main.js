02dae4b78880dfedaeeb70961b8556583f879188
devOwen <admin@owendev.dev>
2020-06-06 21:54:06 -0400

    Added discord embeds to make it look nicer

--- main.js
+++ main.js
@@ -8,12 +8,29 @@ client.on('message', message => {
         let idOfMessage = message.content.toString().split(' ')[1]
         var options = {
             uri: 'https://vlo.to/user/get?discordId=' + idOfMessage,
-            headers: { "Authorization": "" }   // check  https://admin.velo.gg/admin/profile for auth header
+            headers: { "Authorization": "" }, // check  https://admin.velo.gg/admin/profile for auth header
+            json: true,
+            resolveWithFullResponse: true
         }
         rp.get(options)
-        .then (function(response) {
-            message.channel.send(response)
+        .then (function(response, body) {
+            console.log(response.body)
+            const exampleEmbed = new Discord.MessageEmbed()
+            .setColor('#FFFFFF')
+            .addFields(
+                {name: 'email', value: (response.body).user.email},
+                {name: 'key', value: (response.body).user.key},
+                {name: 'discordId', value: (response.body).user.discordId},
+                {name: 'expired', value: (response.body).user.expired},
+                {name: 'status', value: (response.body).user.subscription.status},
+                {name: 'price', value: (response.body).user.subscription.price},
+                {name: 'next_renewal_timestamp', value: (response.body).user.subscription.next_renewal_timestamp},
+                {name: 'next_renewal', value: (response.body).user.subscription.next_renewal},
+                {name: 'joinedDate', value: (response.body).user.joinedDate},
+            )
+            .setFooter('Owen#0021')
+            message.reply(exampleEmbed);
         })
     }
 })
-client.login('token') //bot token here
+client.login('') //bot token here
